98ee29ba7c18b9b990fe9b3763f58d3eba2dbcb6
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-09-05 23:46:18 -0700

    AUTHORS

---
+++ gomba_muthuri_betty.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the Task doers.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("AMOS MUTHURI AND BRIAN GOMBA DID THIS TASK,\n");
+	printf("SEPT 2023.\n");
+	return (0);
+}
